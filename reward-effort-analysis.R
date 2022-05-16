72d5482ad6718f90703852c8b3dab9d4fb0f4fa9
robbiequarter <rj.qrtr@comcast.net>
2022-05-16 09:09:24 -0600

    Build more sophisticated GLMER models with Gamma distribution and log link. Center and scale TRIAL predictor.

--- reward-effort-analysis.R
+++ reward-effort-analysis.R
@@ -3,7 +3,7 @@
 ### Import Libraries
   pacman::p_load(knitr, tidyverse, magrittr, kableExtra, GGally, 
                  car, lmerTest, lme4, rstatix, ggpubr, atable, cowplot, 
-                 sjPlot)
+                 sjPlot, glmmTMB)
 
 ### Import and clean data
   rm(list=ls())
@@ -35,6 +35,7 @@
   # Add Trial column
   trial <- seq(1,400)
   R.exp$TRIAL <- rep(trial, n*2)
+  R.exp$sTRIAL <- scale(R.exp$TRIAL) # scaled trial
   
   # For later use in binning reaches
   R.exp2 <- R.exp
@@ -48,7 +49,7 @@
   pvals <- data.frame(measure = character(), beta = character(), pval = double(), rank=integer(), apval = double())
   
   # Colors  
-  tuftePal <- c("#367a37", "#cc6600", "#333333","#990033")
+  #tuftePal <- c("#367a37", "#cc6600", "#333333","#990033")
   #hist(c(1:5), col=tuftePal)
   
   # Binning every 4 reaches, average 3 NRWD, keep 1 RWD 
@@ -98,54 +99,79 @@
     plot_model(pv.lmer, type = 'int') # plot interaction 
     
     # model 2
-    pv.lmer2 <- lmer(PEAKV ~ TRIAL*MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
+    pv.lmer2 <- lmer(PEAKV ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, REML = FALSE)
     summary(pv.lmer2)
     
     #model 3
     pv.lmer3 <- glmer(PEAKV ~ MASS*REWD + (1|ID), data = R.exp, family=Gamma(log))
-    summary(pv.lmer3)    
+    summary(pv.lmer3)
+    plot_model(pv.lmer3, vline.color = "black")
+    plot_model(pv.lmer3, type = "int")
+    plot_model(pv.lmer3, type = "re", vline.color="black")
+    plot_model(pv.lmer3, type = "pred")
+    
+    pv.lmer4 <- glmer(PEAKV ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, family=Gamma(log))
+    summary(pv.lmer4)
   
   # Reaction time
     # initial model
     rxn.lmer <-  lmer(RXNEX ~ MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
     summary(rxn.lmer)
-    plot_model(rxn.lmer)
- 
+
     # model 2
-    rxn.lmer2 <-  lmer(RXNEX ~ TRIAL*MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
+    rxn.lmer2 <-  lmer(RXNEX ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, REML = FALSE)
     summary(rxn.lmer2)
     
     # model 3
     rxn.lmer3 <- glmer(RXNEX ~ MASS*REWD + (1|ID), data = R.exp, family=Gamma(log))
-    summary(rxn.lmer3)   
+    summary(rxn.lmer3)
+    plot_model(rxn.lmer3, vline.color = "black")
+    plot_model(rxn.lmer3, type = "int")
+    plot_model(rxn.lmer3, type = "re", vline.color="black")
+    plot_model(rxn.lmer3, type = "pred")
+    
+    rxn.lmer4 <- glmer(RXNEX ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, family=Gamma(log))
+    summary(rxn.lmer4)
     
   # Movement time
     # initial model
     dur.lmer <-  lmer(MVTDUR ~ MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
     summary(dur.lmer)
-    plot(dur.lmer)
-    
+
     # model 2
-    dur.lmer2 <-  lmer(MVTDUR ~ TRIAL*MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
+    dur.lmer2 <-  lmer(MVTDUR ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, REML = FALSE)
     summary(dur.lmer2)
     
     # model 3
     dur.lmer3 <- glmer(MVTDUR ~ MASS*REWD + (1|ID), data = R.exp, family=Gamma(log))
     summary(dur.lmer3) 
+    plot_model(dur.lmer3, vline.color = "black")
+    plot_model(dur.lmer3, type = "int")
+    plot_model(dur.lmer3, type = "re", vline.color="black")
+    plot_model(dur.lmer3, type = "pred")
+    
+    dur.lmer4 <- glmer(MVTDUR ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, family=Gamma(log))
+    summary(dur.lmer4)
     
   # Overall vigor
     # initial model
     vig.lmer <-  lmer(VIG ~ MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
     summary(vig.lmer)
-    plot(vig.lmer)
-    
+
     # model 2
-    vig.lmer2 <-  lmer(VIG ~ TRIAL*MASS*REWD + (1|ID), data = R.exp, REML = FALSE)
+    vig.lmer2 <-  lmer(VIG ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, REML = FALSE)
     summary(vig.lmer2)  
    
     # model 3
     vig.lmer3 <- glmer(VIG ~ MASS*REWD + (1|ID), data = R.exp, family=Gamma(log))
     summary(vig.lmer3)   
+    plot_model(vig.lmer3, vline.color = "black")
+    plot_model(vig.lmer3, type = "int")
+    plot_model(vig.lmer3, type = "re", vline.color="black")
+    plot_model(vig.lmer3, type = "pred")
+    
+    vig.lmer4 <- glmer(VIG ~ MASS*REWD + MASS*sTRIAL + REWD*sTRIAL + (1|ID), data = R.exp, family=Gamma(log))
+    summary(vig.lmer4)
   
   
   
--- young-old-analysis.R
+++ young-old-analysis.R
@@ -3,7 +3,7 @@
 ### Import Libraries
   pacman::p_load(knitr, tidyverse, magrittr, kableExtra, GGally, 
                  car, lmerTest, lme4, rstatix, ggpubr, atable, cowplot, 
-                 sjPlot)
+                 sjPlot, glmmTMB)
 
 ### Import and clean data
   #Delete all variables
@@ -22,7 +22,7 @@
   df$Old %<>% as.factor()
   df$Subj %<>% as.factor()
   df$Block %<>% as.factor()
-  
+  df$sTrial <- scale(df$Trial) # scaled & centered trial variable
   
   
 ### Plot distribution of dependent variables
@@ -48,43 +48,67 @@
   
   
   
-  ### Statistical Analyses for Young/Old experiment
-    # Vigor
+### Statistical Analyses for Young/Old experiment
+  # Vigor
     vig.lmer <- lmer(VIG ~ Reward*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(vig.lmer)
     
-    vig.lmer2 <- lmer(VIG ~ Reward*Old*Trial + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
+    vig.lmer2 <- lmer(VIG ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(vig.lmer2)
     
-    # Reaction time
+    vig.lmer3 <- glmer(VIG ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, family = Gamma(log))
+    summary(vig.lmer3)
+    plot_model(vig.lmer3, vline.color = "black")
+    plot_model(vig.lmer3, type = "int")
+    plot_model(vig.lmer3, type = "re", vline.color="black")
+  
+  # Reaction time
     rxn.lmer <- lmer(Rxn ~ Reward*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(rxn.lmer)
- 
-    rxn.lmer2 <- lmer(Rxn ~ Reward*Old*Trial + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
+  
+    rxn.lmer2 <- lmer(Rxn ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(rxn.lmer2)
     
-    rxn.lmer3 <- glmer(Rxn ~ Reward*Old + (1 | Subj), data=df, family = Gamma(log))
+    rxn.lmer3 <- glmer(Rxn ~ Reward*Old+ (1 | Subj), data=df, family = Gamma(log))
     summary(rxn.lmer3)
-    
-    # Duration
+    plot_model(rxn.lmer3, vline.color = "black")
+    plot_model(rxn.lmer3, type = "int")
+    plot_model(rxn.lmer3, type = "re", vline.color="black")
+    #plot_model(rxn.lmer3, type = "pred")
+  
+    rxn.lmer4 <- glmer(Rxn ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, family = Gamma(log))
+
+  # Duration
     dur.lmer <- lmer(Duration ~ Reward*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(dur.lmer)
     
-    dur.lmer2 <- lmer(Duration ~ Reward*Old*Trial + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
+    dur.lmer2 <- lmer(Duration ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(dur.lmer2)
     
     dur.lmer3 <- glmer(Duration ~ Reward*Old + (1 | Subj), data=df, family = Gamma(log))
     summary(dur.lmer3)
-    plot(dur.lmer3)
-  
-    # Peak velocity
+    plot_model(dur.lmer3, vline.color = "black")
+    plot_model(dur.lmer3, type = "int")
+    plot_model(dur.lmer3, type = "re", vline.color="black")
+    plot_model(dur.lmer3, type = "pred")
+    
+    dur.lmer4 <- glmer(Duration ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, family = Gamma(log))
+    summary(dur.lmer4)
+    
+  # Peak velocity
     pv.lmer <- lmer(PeakV ~ Reward*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(pv.lmer)
     
-    pv.lmer2 <- lmer(PeakV ~ Reward*Old*Trial + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
+    pv.lmer2 <- lmer(PeakV ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, REML=FALSE, na.action=na.omit)
     summary(pv.lmer2)
     
     pv.lmer3 <- glmer(PeakV ~ Reward*Old + (1 | Subj), data=df, family = Gamma(log))
     summary(pv.lmer3)
+    plot_model(pv.lmer3, vline.color = "black")
+    plot_model(pv.lmer3, type = "int")
+    plot_model(pv.lmer3, type = "re", vline.color="black")
+    plot_model(pv.lmer3, type = "pred")
+    
+    pv.lmer4 <- glmer(PeakV ~ Reward*Old + sTrial*Reward + sTrial*Old + (1 | Subj), data=df, family = Gamma(log))
 
   
